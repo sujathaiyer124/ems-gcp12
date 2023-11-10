f06e923b2f2ffe299ba82a03799933f09b95c21c
Sujatha Iyer <sujathaiyer124@gmail.com>
2023-11-10 16:09:14 +0530

    Function name change

--- reademp.go
+++ reademp.go
@@ -15,10 +15,10 @@ import (
 )
 
 func init() {
-	functions.HTTP("Reademployee", ReadEmployees)
+	functions.HTTP("Listemployee", ListEmployees)
 }
 
-func ReadEmployees(w http.ResponseWriter, r *http.Request) {
+func ListEmployees(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
 	projectID := "excellent-math-403109"
 	firestoreClient, err := firestore.NewClient(ctx, projectID)
