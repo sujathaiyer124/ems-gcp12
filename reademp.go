9fb07e6eab7d3d9fa20ede8eb3c7ef8d5b99de00
Sujatha Iyer <sujathaiyer124@gmail.com>
2023-11-10 16:01:46 +0530

    Added code for search employee

--- reademp.go
+++ reademp.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/sujathaiyer124/ems-gcp12/models"
 
 	"cloud.google.com/go/firestore"
@@ -13,30 +14,49 @@ import (
 	"google.golang.org/api/iterator"
 )
 
-var firestoreClient *firestore.Client
-
 func init() {
 	functions.HTTP("Reademployee", ReadEmployees)
-	ctx := context.Background()
-	projectID := "excellent-math-403109"
-	Client, err := firestore.NewClient(ctx, projectID)
-	if err != nil {
-		log.Println(err)
-	}
-	firestoreClient = Client
-
 }
 
 func ReadEmployees(w http.ResponseWriter, r *http.Request) {
 	ctx := context.Background()
+	projectID := "excellent-math-403109"
+	firestoreClient, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Println(err)
+	}
 	if firestoreClient == nil {
 		log.Println("Firestore client is not initialized")
 		return
 	}
-	//firestoreClient.Close()
-	iter := firestoreClient.Collection("employees").Documents(ctx)
-	defer iter.Stop()
+	var iter *firestore.DocumentIterator
+	params := mux.Vars(r)
+	fname := params["firstname"]
+	empID := params["id"]
+	lname := params["lastname"]
+	emprole := params["role"]
+	emp_email := params["email"]
 
+	if fname != "" && empID == "" && lname == "" && emprole == "" && emp_email == "" {
+		iter = firestoreClient.Collection("employees").Where("firstname", "==", fname).Documents(ctx)
+		defer iter.Stop()
+	} else if fname == "" && empID == "" && lname != "" && emprole == "" && emp_email == "" {
+		iter = firestoreClient.Collection("employees").Where("lastname", "==", lname).Documents(ctx)
+		defer iter.Stop()
+	} else if fname == "" && empID == "" && lname == "" && emprole == "" && emp_email != "" {
+		iter = firestoreClient.Collection("employees").Where("email", "==", emp_email).Documents(ctx)
+		defer iter.Stop()
+	} else if fname == "" && empID == "" && lname == "" && emprole != "" && emp_email == "" {
+		iter = firestoreClient.Collection("employees").Where("role", "==", emprole).Documents(ctx)
+		defer iter.Stop()
+	} else if fname == "" && empID != "" && lname == "" && emprole == "" && emp_email == "" {
+		iter = firestoreClient.Collection("employees").Where("id", "==", empID).Documents(ctx)
+		defer iter.Stop()
+	} else {
+		iter = firestoreClient.Collection("employees").Documents(ctx)
+		defer iter.Stop()
+	}
+	var found bool
 	var emp []models.Employee
 	for {
 		doc, err := iter.Next()
@@ -65,12 +85,14 @@ func ReadEmployees(w http.ResponseWriter, r *http.Request) {
 		emp = append(emp, employee)
 	}
 	jsonData, err := json.Marshal(emp)
+	if !found {
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode("Not found")
+	}
 	if err != nil {
 		log.Fatalf("Error encoding JSON: %s", err.Error())
 	}
 	w.WriteHeader(http.StatusOK)
 	w.Write(jsonData)
-
-	//defer firestoreClient.Close()
 
 }
